93141c0598e4f34a65e6ab2a0579cfde0b0a3be5
Daniel Hemmerich <daniel.hemmerich@eqno.net>
2023-02-18 19:26:33 +0100

    Setup and Producer Example.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.tutorial"
@@ -10,8 +11,13 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.apache.kafka:kafka-clients:3.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+application {
+    mainClassName = "org.tutorial.Main"
 }
 
 tasks.getByName<Test>("test") {
